be9fadf57416b54bf796dca68213d143485584da
ahanuszczak <hanyhusajn@gmail.com>
2023-03-23 12:27:13 +0100

    Themes and Toolbar

--- app/src/main/java/com/hanuszczak/asteroidradar/view/main/MainActivity.kt
+++ app/src/main/java/com/hanuszczak/asteroidradar/view/main/MainActivity.kt
@@ -2,12 +2,15 @@ package com.hanuszczak.asteroidradar.view.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.hanuszczak.asteroidradar.R
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var toolbar: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 }
