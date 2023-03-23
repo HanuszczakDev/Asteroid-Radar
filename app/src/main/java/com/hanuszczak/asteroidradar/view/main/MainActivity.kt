d29b92b6bc440bee89680d34bfa9f0bf6b16f098
ahanuszczak <hanyhusajn@gmail.com>
2023-03-23 21:36:37 +0100

    Refactor Backround Worker

--- app/src/main/java/com/hanuszczak/asteroidradar/view/main/MainActivity.kt
+++ app/src/main/java/com/hanuszczak/asteroidradar/view/main/MainActivity.kt
@@ -3,14 +3,45 @@ package com.hanuszczak.asteroidradar.view.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.work.*
 import com.hanuszczak.asteroidradar.R
+import com.hanuszczak.asteroidradar.work.RefreshDataWork
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+
     private lateinit var toolbar: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+        delayedInit()
+    }
+
+    private fun delayedInit() = applicationScope.launch { setupRecurringWork() }
+
+    private fun setupRecurringWork() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresBatteryNotLow(true)
+            .setRequiresCharging(true)
+            .apply {
+                setRequiresDeviceIdle(true)
+            }.build()
+
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWork>(1, TimeUnit.DAYS)
+            .setConstraints(constraints)
+            .build()
+
+        WorkManager.getInstance(applicationContext).enqueueUniquePeriodicWork(
+            RefreshDataWork.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest)
     }
 }
