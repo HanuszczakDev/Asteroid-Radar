c9ee441f0314bce0284af35ca6f7e70a6be84abb
ahanuszczak <hanyhusajn@gmail.com>
2023-04-23 02:32:27 +0200

    Nasa API KEY

--- app/src/main/java/com/hanuszczak/asteroidradar/util/Constants.kt
+++ app/src/main/java/com/hanuszczak/asteroidradar/util/Constants.kt
@@ -5,5 +5,5 @@ object Constants {
     const val SEVEN_DAYS = 7
     const val ONE_DAY = 1
     const val BASE_URL = "https://api.nasa.gov/"
-    const val API_KEY = "XXX"
+    const val API_KEY = "<NASA_API_KEY>"
 }
